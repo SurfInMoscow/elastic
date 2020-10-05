79c8bbb76dd98f68471c539fc6733bfc5a63d689
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-10-05 12:32:22 +0300

    logback + elastic core dependencies

--- src/main/java/com/voroby/elasticclient/ElasticClientApplication.java
+++ src/main/java/com/voroby/elasticclient/ElasticClientApplication.java
@@ -1,13 +1,23 @@
 package com.voroby.elasticclient;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ElasticClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ElasticClientApplication.class, args);
+    }
+
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+        return new RestHighLevelClient(RestClient
+                .builder(new HttpHost("localhost", 9200, "http")));
     }
 
 }
