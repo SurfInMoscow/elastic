80f8e14425ddddc9ff97b496cece98b413f820f1
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-10-05 11:14:37 +0300

    initial commit

---
+++ src/main/java/com/voroby/elasticclient/ElasticClientApplication.java
@@ -0,0 +1,13 @@
+package com.voroby.elasticclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voroby/elasticclient/ElasticClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voroby.elasticclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
