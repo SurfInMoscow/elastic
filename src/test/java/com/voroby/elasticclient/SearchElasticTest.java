2adf64b480661e78a441c3f0ec943408042aabe4
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-10-16 14:42:25 +0300

    SearchElasticRequest with match queries

--- src/test/java/com/voroby/elasticclient/SearchElasticTest.java
+++ src/test/java/com/voroby/elasticclient/SearchElasticTest.java
@@ -1,5 +1,6 @@
 package com.voroby.elasticclient;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.voroby.elasticclient.domain.Item;
 import com.voroby.elasticclient.domain.User;
@@ -11,6 +12,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -19,9 +21,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SearchElasticTest extends AbstractElasticTest {
     private static List<User> users = new ArrayList<>();
@@ -48,19 +53,53 @@ public class SearchElasticTest extends AbstractElasticTest {
     }
 
     @Test
-    public void updateByQueryTest() throws IOException {
+    public void updateByQueryTest() throws IOException, InterruptedException {
         index();
+        /*don't use this delay! I did it because my test computer is slow and
+        local elastic didn't finished indexing before search query starts.*/
+        Thread.sleep(2000);
         SearchRequest searchRequest = new SearchRequest();
         searchRequest.indices("users", "items");
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        sourceBuilder.query(QueryBuilders.matchAllQuery());
-        sourceBuilder.size(200);
+        /*
+        To source simple match query by single value for filed. It's also possible to add several values check.
+        sourceBuilder.query(QueryBuilders.matchQuery("name", "GetItem2"));
+
+        Alternative variant.
+        MatchQueryBuilder match = new MatchQueryBuilder("name", "GetItem2");
+
+        In this case we refer to field in nested object in our document.
+        MatchQueryBuilder match1 = new MatchQueryBuilder("items.name", "GetItem2");
+        */
+
+        //add match query for value in several fields in our indices.
+        MultiMatchQueryBuilder queryBuilder = QueryBuilders.multiMatchQuery("GetItem2", "name", "items.name");
+        sourceBuilder.query(queryBuilder);
+        sourceBuilder.size(100);
         searchRequest.source(sourceBuilder);
         SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
 
         SearchHits hits = searchResponse.getHits();
         List<SearchHit> list = Arrays.asList(hits.getHits());
-        System.out.println(list.size());
+        List<User> foundUsers = new ArrayList<>();
+        List<Item> foundItems = new ArrayList<>();
+        list.forEach(hit -> {
+            String hitAsString = hit.getSourceAsString();
+            Gson gson;
+            switch (hit.getIndex()) {
+                case "users":
+                    gson = getGsonWithTypeAdapter(User.class, new UserJsonAdapter());
+                    foundUsers.add(gson.fromJson(hitAsString, User.class));
+                    break;
+                case "items":
+                    gson = getGsonWithTypeAdapter(Item.class, new ItemJsonAdapter());
+                    foundItems.add(gson.fromJson(hitAsString, Item.class));
+                    break;
+            }
+        });
+
+        assertTrue(foundUsers.stream().anyMatch(found -> found.equals(users.get(0))));
+        assertTrue(foundItems.stream().anyMatch(found -> found.equals(items.get(1))));
     }
 
     private void index() throws IOException {
@@ -88,5 +127,12 @@ public class SearchElasticTest extends AbstractElasticTest {
             bulkRequest.add(request);
         });
         restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+    }
+
+    private Gson getGsonWithTypeAdapter(Type type, Object typeAdapter) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(type, typeAdapter);
+
+        return  builder.create();
     }
 }
